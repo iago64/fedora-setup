8f24910022f2f4a636c103e649a5c7f118e2a57b
Damian Facchini <facchinidm@gmail.com>
2024-12-06 11:01:05 -0300

    feat: update java, add net 8 and node

--- paquetes.sh
+++ paquetes.sh
@@ -3,12 +3,12 @@ sudo dnf update
 
 # Paquetes Herramientas
 echo "Installando Herramientas Generales"
-sudo dnf install vim emacs nano strace openssh curl htop tree wget terminator xclip bless neofetch net-tools openssh-server
+sudo dnf install vim emacs nano strace openssh curl htop tree wget terminator xclip bless fastfetch net-tools openssh-server
 
 
 # Lenguajes de Programacion
 echo "Installando Lenguajes y Herramientas de Programacion"
-sudo dnf install kernel-devel kernel-headers gcc  gcc-c++ gdb  python3  python3-pip java-17-openjdk CUnit make  cmake 
+sudo dnf install kernel-devel kernel-headers gcc  gcc-c++ gdb  python3  python3-pip java-21-openjdk dotnet-sdk-8.0 CUnit make  cmake 
 
 
 # DEV Utils
