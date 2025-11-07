09496a2331bfb09f3cc689e086aae62ad1f7f86e
Damian Facchini <facchinidm@gmail.com>
2025-11-07 17:49:44 -0300

    feat: add Jetbrains toolbox y jq

--- paquetes.sh
+++ paquetes.sh
@@ -8,7 +8,7 @@ sudo dnf -y update
 # --- Herramientas básicas ---
 sudo dnf install -y \
     vim emacs nano strace openssh curl htop tree wget terminator \
-    xclip bless fastfetch net-tools openssh-server valgrind meld
+    xclip bless fastfetch net-tools openssh-server valgrind meld jq
 
 # --- Lenguajes y compiladores ---
 sudo dnf install -y \
@@ -64,6 +64,12 @@ gpgkey=https://packages.microsoft.com/keys/microsoft.asc
 EOF
 sudo dnf -y install code
 
+# --- JetBrains Toolbox ---
+
+TBA_LINK=$(curl -fsSL "https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release" | jq -r '.TBA[0].downloads.linux.link')
+wget -qO- "${TBA_LINK:?}" | sudo tar xvzC /opt
+/opt/jetbrains-toolbox-*/bin/jetbrains-toolbox
+
 # --- Flatpak + Flathub ---
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y --noninteractive flathub \
@@ -71,6 +77,7 @@ flatpak install -y --noninteractive flathub \
     com.getpostman.Postman \
     com.obsproject.Studio \
     org.libretro.RetroArch \
+    org.videolan.VLC \
     com.bitwarden.desktop || true
 
 # --- ZSH + Oh My Zsh ---
@@ -78,3 +85,27 @@ sudo dnf -y install zsh
 if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
     RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
+
+# --- Integrar configuraciones de entorno en Zsh ---
+ZSHRC="$HOME/.zshrc"
+
+{
+    echo ""
+    echo "# --- Entorno de desarrollo personalizado ---"
+    echo "# NVM / Node.js"
+    echo 'export NVM_DIR="$HOME/.nvm"'
+    echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"'
+    echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"'
+    echo ""
+    echo "# PNPM"
+    echo 'export PATH="$HOME/.local/share/pnpm:$PATH"'
+    echo ""
+    echo "# SDKMAN"
+    echo 'export SDKMAN_DIR="$HOME/.sdkman"'
+    echo '[[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"'
+    echo ""
+    echo "# Java"
+    echo 'export JAVA_HOME="/usr/lib/jvm/temurin-21-jdk"'
+    echo 'export PATH="$JAVA_HOME/bin:$PATH"'
+    echo ""
+} >> "$ZSHRC"
