d44f71dbbccc01b60725e4e4e25bb4d0d8f2bf20
Damian Facchini <facchinidm@gmail.com>
2026-03-09 10:10:15 -0300

    feat: update de paquetes

--- paquetes.sh
+++ paquetes.sh
@@ -13,26 +13,17 @@ sudo dnf install -y \
 # --- Lenguajes y compiladores ---
 sudo dnf install -y \
     kernel-devel kernel-headers gcc gcc-c++ gdb python3 python3-pip \
-    adoptium-temurin-java-repository temurin-21-jdk \
-    dotnet-sdk-8.0 CUnit make cmake
-
-# Configurar Java (solo si hay más de una versión)
-if [[ $(update-alternatives --list java | wc -l) -gt 1 ]]; then
-    sudo update-alternatives --config java
-fi
+    adoptium-temurin-java-repository dotnet-sdk-10.0 \
+    CUnit make cmake
 
 # --- SDKMAN ---
 if [[ ! -d "$HOME/.sdkman" ]]; then
     curl -s "https://get.sdkman.io" | bash
     source "$HOME/.sdkman/bin/sdkman-init.sh"
+    sdk install java 21.0.10-amzn
+    sdk install java 25.0.2-amzn
     sdk install quarkus
     sdk install gradle
-fi
-
-# Configurar JAVA_HOME global (si no existe)
-JAVA_CONF="/etc/java/maven.conf"
-if ! grep -q "JAVA_HOME" "$JAVA_CONF" 2>/dev/null; then
-    echo "JAVA_HOME=/usr/lib/jvm/temurin-21-jdk" | sudo tee "$JAVA_CONF" >/dev/null
 fi
 
 # --- NVM / Node.js / PNPM ---
@@ -65,7 +56,6 @@ EOF
 sudo dnf -y install code
 
 # --- JetBrains Toolbox ---
-
 TBA_LINK=$(curl -fsSL "https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release" | jq -r '.TBA[0].downloads.linux.link')
 wget -qO- "${TBA_LINK:?}" | sudo tar xvzC /opt
 /opt/jetbrains-toolbox-*/bin/jetbrains-toolbox
@@ -74,7 +64,7 @@ wget -qO- "${TBA_LINK:?}" | sudo tar xvzC /opt
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y --noninteractive flathub \
     com.discordapp.Discord \
-    com.getpostman.Postman \
+    com.usebruno.Bruno \
     com.obsproject.Studio \
     org.libretro.RetroArch \
     org.videolan.VLC \
